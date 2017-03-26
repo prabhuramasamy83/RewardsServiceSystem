b185047cd8f7ea369d64c0dc8618d5faca7aef38
prabhuramasamy83 <mindmee@gmail.com>
2017-03-27 00:08:31 +0100

    Create EligibilityStatuses.java

---
+++ src/main/java/com/skytest/constant/EligibilityStatuses.java
@@ -0,0 +1,13 @@
+/**
+ * enum holds the customer eligibility statuses
+ */
+package com.skytest.constant;
+
+/**
+ * @author Prabhu Ramasamy
+ *
+ */
+public enum EligibilityStatuses {
+	CUSTOMER_ELIGIBLE,
+	CUSTOMER_INELIGIBLE
+}
